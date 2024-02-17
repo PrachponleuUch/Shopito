40e03dc3880430749ff0bd90e66a8a50e22fb162
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-17 15:40:36 +1100

    Feature: added header and logo

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import styles from '../../styles/Header.module.scss'
+import { Link } from 'react-router-dom'
+
+const logo = (
+  <div className={styles.logo}>
+    <Link to='/'>
+      <h2>
+        Shop<span>ito</span>
+      </h2>
+    </Link>
+  </div>
+)
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <div className={styles.header}>
+        {logo}
+      </div>
+    </header>
   )
 }
 
