5d3606cc1fc100208b703a018aff407a3c83c842
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-19 15:09:14 +1100

    Feature: added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/home/home";
 import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 const App = () => {
   return (
     <>
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home/>}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   );
