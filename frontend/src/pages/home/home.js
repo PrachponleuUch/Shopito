04ef9903bd431890be8d61e63c9f26cc1ffc41fe
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-19 16:40:15 +1100

    Feature: added slider

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Slider from '../../components/slider/slider'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <Slider/>
+    </>
   )
 }
 
