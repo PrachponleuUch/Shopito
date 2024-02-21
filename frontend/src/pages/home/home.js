d593678941bb646340bc3b698521f950188d0d22
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-21 12:49:11 +1100

    Feature: added info boxes on Home Page

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Slider from '../../components/slider/slider'
+import styles from '../../styles/Home.scss'
+import HomeInfoBox from './homeInfoBox'
 
 const Home = () => {
   return (
     <>
       <Slider/>
+      <section>
+        <div className='container'>
+          <HomeInfoBox/>
+        </div>
+      </section>
     </>
   )
 }
