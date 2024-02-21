313d0029db4c29d0763bcb2c3a86889b235d68ce
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-21 14:37:29 +1100

    Feature: added product carousels to Home Page

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -2,14 +2,49 @@ import React from 'react'
 import Slider from '../../components/slider/slider'
 import styles from '../../styles/Home.scss'
 import HomeInfoBox from './homeInfoBox'
+import { productData } from '../../components/carousel/data'
+import CarouselItem from '../../components/carousel/carouselItem'
+import ProductCarousel from '../../components/carousel/carousel'
+import ProductCategory from './productCategory'
+const PageHeading = ({ heading, btnText }) => {
+  return (
+    <>
+      <div className='--flex-between'>
+        <h2 className='--fw-thin'>{heading}</h2>
+        <button className='--btn'>{btnText}</button>
+      </div>
+    <div className='--hr'></div>
+    </>
+  )
+}
 
 const Home = () => {
+  const productss = productData.map((item) => {
+    return (
+      <div key={item.id} >
+        <CarouselItem
+          name={item.name}
+          url={item.imageurl}
+          price={item.price}
+          description={item.description}
+        />
+      </div>
+    )
+  })
   return (
     <>
       <Slider/>
       <section>
         <div className='container'>
           <HomeInfoBox/>
+          <PageHeading heading={"Latest Product"} btnText={"Shop Now >>>"}/>
+          <ProductCarousel products={productss}/>
+        </div>
+      </section>
+      <section className='--bt-grey'>
+        <div className='container'>
+          <h3>Categories</h3>
+          <ProductCategory/>
         </div>
       </section>
     </>
