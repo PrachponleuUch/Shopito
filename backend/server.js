39c1c0662f92405999b0964c776233d98a5c4e68
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-22 12:14:47 +1100

    Feature: connected to MongoDB  and added middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,36 @@
-console.log("Hello World!!!");
+const dotenv = require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}))
+app.use(
+  cors({
+  origin: ["http://localhost:3000", "https://shopito-alex.vercel.app"],
+  credential: true
+}))
+
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page...")
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
