fa23d99cced8519187aaf45f1d17d5c76af92d6a
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-23 16:12:51 +1100

    Feature: added a function to get user login status

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -135,9 +135,25 @@ const getUser = asyncHandler(async (req, res) => {
   }
 })
 
+// Get Login Status
+const getLoginStatus = asyncHandler (async (req, res) => {
+  const token = req.cookies.token
+  if (!token){
+    return res.json(false)
+  }
+  
+  // Verify token
+  const verified = jwt.verify(token, process.env.JWT_SECRET)
+  if (verified){
+    return res.json(true)
+  }
+  return res.json(false)
+})
+
 module.exports = {
   registerUser,
   loginUser,
   logoutUser,
-  getUser
+  getUser,
+  getLoginStatus
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,14 @@
 const express = require("express")
-const { registerUser, loginUser, logoutUser, getUser } = require("../controllers/userController")
+const { registerUser, loginUser, logoutUser, getUser, getLoginStatus } = require("../controllers/userController")
 const { protect } = require("../middleware/authMiddleware")
 const router = express.Router()
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.get("/logout", logoutUser)
-router.get("/get", protect, getUser)
+router.get("/getUser", protect, getUser)
+router.get("/getLoginStatus", getLoginStatus)
+
 
 
 module.exports = router
