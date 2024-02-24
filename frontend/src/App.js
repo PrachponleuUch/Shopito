55746ea35db3110eb96ef2e2a29f37abd0ffbb80
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-24 12:51:35 +1100

    Feature: added spinner/loadingscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/home/home";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import { Spinner } from "./components/loader/loader";
+
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Header/>
+        <Spinner/>
         <Routes>
           <Route path="/" element={<Home/>}/>
         </Routes>
