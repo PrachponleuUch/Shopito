176b57f0f308b652747b170dfcd56f158644dbe9
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-24 14:24:50 +1100

    Feature: added register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/home/home";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import { Spinner } from "./components/loader/loader";
 import Register from "./pages/auth/register";
 import Login from "./pages/auth/login";
 
@@ -12,7 +11,6 @@ const App = () => {
     <>
       <BrowserRouter>
         <Header/>
-        <Spinner/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
--- frontend/src/pages/auth/register.js
+++ frontend/src/pages/auth/register.js
@@ -1,10 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../../styles/auth.module.scss'
+import registerImg from '../../assets/register.png'
+import Card from '../../components/card/card'
+import { Link } from 'react-router-dom'
+
+const initialState = {
+  name : "",
+  email : "",
+  password : "",
+  cPassword : ""
+}
+
 const Register = () => {
+  const [formData, setFormData] = useState(initialState)
+  const { name, email, password, cPassword } = formData
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+  const registerUser = () => { }
   return (
-    <div>
-      
-    </div>
+    <section className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <h2>Register</h2>
+          <form onSubmit={registerUser}>
+            <input 
+                type="text"
+                placeholder='Name'
+                name="name"
+                required
+                value={name}
+                onChange={handleInputChange} 
+            />
+            <input 
+              type="text"
+              placeholder='Email'
+              name={email}
+              required
+              value={email}
+              onChange={handleInputChange} 
+            />
+            <input 
+              type="password"
+              placeholder='Password'
+              name={password}
+              required
+              value={password}
+              onChange={handleInputChange} 
+            />
+            <input 
+              type="text"
+              placeholder='Confirm Password'
+              name="cPassword"
+              required
+              value={cPassword}
+              onChange={handleInputChange} 
+            />
+            <button type='submit' className="--btn --btn-danger --btn-block">
+              Register
+            </button>
+          </form>
+          <span className={styles.register}>
+            <p>Already have an account?</p>
+            <Link to="/login">Login</Link>
+          </span>
+        </div>
+      </Card>
+      <div className={styles.img}>
+        <img src={registerImg} alt="registerImage" width={400} />
+      </div>
+    </section>
   )
 }
 
