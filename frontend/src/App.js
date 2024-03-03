5666165ee8a610aff83c6107172050815a2fbe41
PrachponleuUch <ponleuvuthy@gmail.com>
2024-03-03 15:53:38 +1100

    Feature: added component logic and Redux logic to register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,17 @@ import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Register from "./pages/auth/register";
 import Login from "./pages/auth/login";
-
+import axios from "axios";
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 const App = () => {
+  // Every http request using axios will have withCredentials set to true by default
+  axios.defaults.withCredentials = true
+
   return (
     <>
       <BrowserRouter>
+        <ToastContainer/>
         <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
--- frontend/src/pages/auth/register.js
+++ frontend/src/pages/auth/register.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../../styles/auth.module.scss'
 import registerImg from '../../assets/register.png'
 import Card from '../../components/card/card'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { validateEmail } from '../../utils'
+import { useDispatch, useSelector } from "react-redux"
+import { RESET_AUTH, register } from '../../redux/features/auth/authSlice'
+import Loader from '../../components/loader/loader'
 
 const initialState = {
   name : "",
@@ -14,6 +19,9 @@ const initialState = {
 const Register = () => {
   const [formData, setFormData] = useState(initialState)
   const { name, email, password, cPassword } = formData
+  const dispatch = useDispatch()
+  const { isLoading, isLoggedIn, isSuccess } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
 
   const handleInputChange = (e) => {
     const {name, value} = e.target
@@ -22,59 +30,96 @@ const Register = () => {
       [name]: value
     })
   }
-  const registerUser = () => { }
+
+  const registerUser = async (e) => { 
+    e.preventDefault() 
+    // console.log(name, email, password, cPassword)
+    if (!email || !password) {
+      return toast.error("All fields are required.")
+    }
+    if (password.length < 6) {
+      return toast.error("Password must be 6 characters or more.")
+    }
+    if (!validateEmail(email)) {
+      return toast.error("Please enter a valid email.")
+    }
+    if (password !== cPassword) {
+      return toast.error("Passwords must match.")
+    }
+
+    const userData = {
+      name,
+      email,
+      password
+    }
+
+    await dispatch(register(userData))
+  }
+
+  useEffect(() => {
+    if (isSuccess && isLoggedIn) {
+      navigate("/")
+    }
+
+    // Refresh states except loggedIn
+    dispatch(RESET_AUTH())
+  }, [isSuccess, isLoggedIn, dispatch, navigate])
+
   return (
-    <section className={`container ${styles.auth}`}>
-      <Card>
-        <div className={styles.form}>
-          <h2>Register</h2>
-          <form onSubmit={registerUser}>
-            <input 
+    <>
+    {(isLoading && <Loader/>)}
+      <section className={`container ${styles.auth}`}>
+        <Card>
+          <div className={styles.form}>
+            <h2>Register</h2>
+            <form onSubmit={registerUser}>
+              <input 
+                  type="text"
+                  placeholder='Name'
+                  name="name"
+                  required
+                  value={name}
+                  onChange={handleInputChange} 
+              />
+              <input 
                 type="text"
-                placeholder='Name'
-                name="name"
+                placeholder='Email'
+                name='email'
                 required
-                value={name}
+                value={email}
                 onChange={handleInputChange} 
-            />
-            <input 
-              type="text"
-              placeholder='Email'
-              name={email}
-              required
-              value={email}
-              onChange={handleInputChange} 
-            />
-            <input 
-              type="password"
-              placeholder='Password'
-              name={password}
-              required
-              value={password}
-              onChange={handleInputChange} 
-            />
-            <input 
-              type="text"
-              placeholder='Confirm Password'
-              name="cPassword"
-              required
-              value={cPassword}
-              onChange={handleInputChange} 
-            />
-            <button type='submit' className="--btn --btn-danger --btn-block">
-              Register
-            </button>
-          </form>
-          <span className={styles.register}>
-            <p>Already have an account?</p>
-            <Link to="/login">Login</Link>
-          </span>
+              />
+              <input 
+                type="password"
+                placeholder='Password'
+                name='password'
+                required
+                value={password}
+                onChange={handleInputChange} 
+              />
+              <input 
+                type="password"
+                placeholder='Confirm Password'
+                name="cPassword"
+                required
+                value={cPassword}
+                onChange={handleInputChange} 
+              />
+              <button type='submit' className="--btn --btn-danger --btn-block">
+                Register
+              </button>
+            </form>
+            <span className={styles.register}>
+              <p>Already have an account?</p>
+              <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </Card>
+        <div className={styles.img}>
+          <img src={registerImg} alt="registerImage" width={400} />
         </div>
-      </Card>
-      <div className={styles.img}>
-        <img src={registerImg} alt="registerImage" width={400} />
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
